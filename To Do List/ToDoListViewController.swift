8b1f110969d1e763e514c020c5b9e5f5b7a4c91b
Olexsii <realcarter50@gmail.com>
2021-10-26 23:03:56 +0300

    Added TableView Delegate Method and Accessories

--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     let ithemArray = ["Olexsii Levchenko", "Sabina Babaeva", "Olga Levchenko"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,16 +19,29 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    // Mark: number Of Rows In Section
+    // Mark: - Number Of Rows In Section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return ithemArray.count
     }
     
-      // Mark: The method is responsible for what should be displayed in our cells.
+    // Mark: The method is responsible for what should be displayed in our cells.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoIthemCell", for: indexPath)
         cell.textLabel?.text = ithemArray[indexPath.row]
         return cell
     }
+    
+    //Mark: - Added TableView Delegate Method and Accessories for selecting a cell when clicked 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        print(ithemArray[indexPath.row])
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        if  cell?.accessoryType == .checkmark {
+            cell!.accessoryType = .none
+        } else {
+            cell!.accessoryType = .checkmark
+        }
+    }
+    
 }
