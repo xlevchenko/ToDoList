9c914c5a16405610ecc3d510c2882e6cb0333679
Olexsii <realcarter50@gmail.com>
2021-10-31 23:46:35 +0200

    Integrated custom checkbox

--- To Do List/Controller/ToDoListViewController.swift
+++ To Do List/Controller/ToDoListViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import M13Checkbox
 
 class ToDoListViewController: UITableViewController {
+    
+    @IBOutlet var toDoListTableView: UITableView!
+    
+    
     
     var ithemArray = ["Olexsii Levchenko", "Sabina Babaeva", "Olga Levchenko"]
     
@@ -15,41 +20,32 @@ class ToDoListViewController: UITableViewController {
         super.viewDidLoad()
         
         //Mark: - Registering a Table View Cell
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoIthemCell")
+        toDoListTableView.delegate = self
+        toDoListTableView.dataSource = self
         
+       
     }
     
     // Mark: - Number Of Rows In Section
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ithemArray.count
     }
-    
+
     // Mark: The method is responsible for what should be displayed in our cells.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoIthemCell", for: indexPath)
-        cell.textLabel?.text = ithemArray[indexPath.row]
+        let cell = toDoListTableView.dequeueReusableCell(withIdentifier: "ToDoIthemCell", for: indexPath) as! TableViewCell
+        
+        let checkbox = M13Checkbox(frame: CGRect(x: 4.0, y: 8.0, width: 30.0, height: 30.0))
+        cell.taskLabel.text = ithemArray[indexPath.row]
+        cell.checkMark.addSubview(checkbox)
         return cell
     }
     
-    //Mark: - Added TableView Delegate Method and Accessories for selecting a cell when clicked
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //        print(ithemArray[indexPath.row])
-        
-        let cell = tableView.cellForRow(at: indexPath)
-        if  cell?.accessoryType == .checkmark {
-            cell!.accessoryType = .none
-        } else {
-            cell!.accessoryType = .checkmark
-        }
-    }
-    
+
     @IBAction func addNewItems(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             //What will happen once the user clicks the Add Item button on our UIAlert."
