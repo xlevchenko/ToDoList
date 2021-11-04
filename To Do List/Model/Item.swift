11d1f872ee4e4d1934aeefd7e8b1ef3202bdcba8
Olexsii <realcarter50@gmail.com>
2021-11-04 22:15:41 +0200

    add UserDefaults

---
+++ To Do List/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  To Do List
+//
+//  Created by Olexsii Levchenko on 11/3/21.
+//
+
+import Foundation
+
+
+class ItemCell {
+    var checkmark: Bool = false
+    var title: String = ""
+    
+}
