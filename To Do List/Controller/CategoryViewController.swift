8ab45770916654028e11b5198a6e31b80a782170
Olexsii <realcarter50@gmail.com>
2021-11-22 17:38:12 +0200

    update category controller, and functionality for adding a new category

--- To Do List/Controller/CategoryViewController.swift
+++ To Do List/Controller/CategoryViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
     
@@ -22,6 +23,9 @@ class CategoryViewController: UITableViewController {
         //Mark: - Registering a Table View Cell
         categoryTableView.delegate = self
         categoryTableView.dataSource = self
+        
+        loadCategory()
+        
     }
 
     //MARK: - TableView DataSource Methods
@@ -34,12 +38,9 @@ class CategoryViewController: UITableViewController {
     
     //The method is responsible for what should be displayed in our cells.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = categoryTableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! TableViewCell
+        let cell = categoryTableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryTableViewCell
         
-        let item = itemCategory[indexPath.row]
-        cell.taskLabel.text = item.name
-        
-        //cell.checkMark.checkState = item.done == true ? .checked : .unchecked
+        cell.categoryLabel.text = itemCategory[indexPath.row].name
         
         return cell
     }
@@ -48,7 +49,7 @@ class CategoryViewController: UITableViewController {
     @IBAction func addButtonPresed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
             
             //What will happen once the user clicks the Add Item button on our UIAlert."
@@ -56,16 +57,44 @@ class CategoryViewController: UITableViewController {
             newCategory.name = textField.text!
             
             self.itemCategory.append(newCategory)
-            //self.saveItem()
+            self.saveCategory()
         }
         
         alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new item"
+            alertTextField.placeholder = "Create category"
             textField = alertTextField
         }
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
-}
+    
+    
+    //MARK: - Save and load data methods
+    func saveCategory() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            itemCategory = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
 
+
+
+    //MARK: - TableView Delegate Method
+    
+    //Tells the delegate a row is selected
+    override  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
